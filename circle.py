ea5f5ebc8f91149140e080051b82714c66a4370c
Polina <polina-k7@mail.ru>
2024-10-11 17:13:01 +0300

    throw to new branch

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    ''' Принимает число r (int) - радиус круга, и возвращает его площадь'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    ''' Принимает число r (int) - радиус круга, и возвращает длину окружности'''
     return 2 * math.pi * r
 
