4897a7276afdae5e96c9a6066396c4d8aa1fe5dc
Polina <polina-k7@mail.ru>
2024-11-22 17:58:56 +0300

    Fixing functions and adding unit tests

--- square.py
+++ square.py
@@ -1,9 +1,12 @@
-
 def area(a):
     ''' Принимает число a (int) - сторону квадрата, и возвращает его площадь'''
+    if a <= 0:
+        raise ValueError("The side of the square must be a positive number")
     return a * a
 
 
 def perimeter(a):
     ''' Принимает число a (int) - сторону квадрата, и возвращает его периметр'''
+    if a <= 0:
+        raise ValueError("The side of the square must be a positive number")
     return 4 * a
